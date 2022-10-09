511373f8137f19059829050f3c08512e10671671
Thorsten Schminkel <thorsten@schminkel.de>
2022-10-09 17:56:24 +0200

    .

--- pages/react-dropzone-uploader.tsx
+++ pages/react-dropzone-uploader.tsx
@@ -25,11 +25,19 @@ const ReactDropzoneUploader = () => {
         />
 
       </div>
-      
+
       <div className='flex justify-center mx-auto w-[600px] p-8'>
-        
-        <div className="p-4 mb-4 text-sm text-red-700 bg-red-100 rounded-lg dark:bg-red-200 dark:text-red-800" role="alert">
-          <span className="font-medium">Error!</span> The area does not update correctly when upload is progressed!
+
+        <div className="flex p-6 mb-4 text-sm text-red-700 bg-red-100 rounded-lg dark:bg-red-200 dark:text-red-800" role="alert">
+        <svg aria-hidden="true" className="flex-shrink-0 inline w-5 h-5 mr-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"></path></svg>
+        <span className="sr-only">Danger</span>
+          <div>
+            <span className="font-medium">NOT working correct:</span>
+              <ul className="mt-1.5 ml-4 text-red-700 list-disc list-inside">
+                <li>The area does not update correctly when upload is progressed!</li>
+                <li>Drag something in the second time and you see the changes.</li>
+            </ul>
+          </div>
         </div>
 
       </div>

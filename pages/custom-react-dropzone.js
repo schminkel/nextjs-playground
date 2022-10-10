fdd53969e92ddb6a6d7596e690b753c6e33978f1
Thorsten Schminkel <thorsten@schminkel.de>
2022-10-10 10:12:45 +0200

    .

--- pages/custom-react-dropzone.js
+++ pages/custom-react-dropzone.js
@@ -97,7 +97,8 @@ export default class UploadFiles extends Component {
 
   onDrop(files) {
     if (files.length > 0) {
-      this.setState({ selectedFiles: files });
+      this.setState({ selectedFiles: files, progressInfos: [], message: [] });
+
     }
   }
 
@@ -111,6 +112,13 @@ export default class UploadFiles extends Component {
 
     return (
       <>
+
+        {/* Divider */}
+        <div className="relative flex py-5 items-center">
+            <div className="flex-grow border-t border-gray-400"></div>
+            <span className="flex-shrink mx-4 text-gray-400">Dropzone Uploader</span>
+            <div className="flex-grow border-t border-gray-400"></div>
+        </div>
 
         {/*  Dropzone Section  */} 
         <div className="flex justify-center mx-auto w-[600px] p-8">
@@ -211,25 +219,39 @@ export default class UploadFiles extends Component {
           )
         }
         
+        {/* Divider */}
+        <div className="relative flex py-5 items-center">
+            <div className="flex-grow border-t border-gray-400"></div>
+            <span className="flex-shrink mx-4 text-gray-400">File List from Server</span>
+            <div className="flex-grow border-t border-gray-400"></div>
+        </div>
 
         {/*  Files List From Server  */} 
         {fileInfos.length > 0 && (
           <div className="flex justify-center mx-auto w-[600px] p-8">
-            <div className="card">
-              <div className="card-header">List of Files</div>
-              <ul className="list-group list-group-flush">
-                {fileInfos &&
-                  fileInfos.map((file, index) => (
-                    <li className="list-group-item" key={index}>
-                      <a href={file.url}>{file.name}</a>
+          <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-2 sm:gap-4 sm:px-6 w-[600px]">
+            <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+              <ul role="list" class="divide-y divide-gray-200 rounded-md border border-gray-200">
+                {fileInfos && fileInfos.map((file, index) => (
+                    <li class="flex items-center justify-between py-3 pl-3 pr-4 text-sm" key={index}>
+                      <div class="flex w-0 flex-1 items-center">
+                        <svg class="h-5 w-5 flex-shrink-0 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                          <path fill-rule="evenodd" d="M15.621 4.379a3 3 0 00-4.242 0l-7 7a3 3 0 004.241 4.243h.001l.497-.5a.75.75 0 011.064 1.057l-.498.501-.002.002a4.5 4.5 0 01-6.364-6.364l7-7a4.5 4.5 0 016.368 6.36l-3.455 3.553A2.625 2.625 0 119.52 9.52l3.45-3.451a.75.75 0 111.061 1.06l-3.45 3.451a1.125 1.125 0 001.587 1.595l3.454-3.553a3 3 0 000-4.242z" clip-rule="evenodd" />
+                        </svg>
+                        <span class="ml-2 w-0 flex-1 truncate">{file.name}</span>
+                      </div>
+                      <div class="ml-4 flex-shrink-0">
+                        <a href={file.url} class="font-medium text-indigo-600 hover:text-indigo-500">Download</a>
+                      </div>
                     </li>
-                  ))}
+                  ))
+                }
               </ul>
-            </div>
+            </dd>
+          </div>
           </div>
           )
         }
-        
 
       </>
     );
--- pages/html-form-uploader.tsx
+++ pages/html-form-uploader.tsx
@@ -69,6 +69,7 @@ const Uploader: NextPage = () => {
 
         <>
 
+        {/* Divider */}
         <div className="relative flex py-5 items-center">
             <div className="flex-grow border-t border-gray-400"></div>
             <span className="flex-shrink mx-4 text-gray-400">HTML Basic Upload Form</span>
